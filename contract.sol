c987fe2c57deb57a1a90a6b4de4a756680abd969
Sulekhajarajapu <99287829+Sulekhajarajapu@users.noreply.github.com>
2022-02-26 21:50:36 +0000

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -4,7 +4,7 @@ contract Greeter {
   string public greeting;
 
   constructor() public {
-    greeting = 'Hello1';
+    greeting = 'Hello';
   }
 
   function setGreeting(string memory _greeting) public {
