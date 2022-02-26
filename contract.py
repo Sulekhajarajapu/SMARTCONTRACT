dca02c18daeb7652c2325367045116fa1cac6733
Sulekhajarajapu <99287829+Sulekhajarajapu@users.noreply.github.com>
2022-02-26 21:48:40 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -6,36 +6,5 @@ from web3.auto import w3
 from web3.providers.websocket import WebsocketProvider
 from web3 import Web3
 from solc import compile_standard
-print("hello world1")
- with open("contract.sol") as c:
- contractText=c.read()
- with open(".pk") as pkfile:
- privateKey=pkfile.read()
-with open(".infura") as infurafile:
- infuraKey=infurafile.read()
-print("file read")
-compiled_sol = compile_standard({
-    "language": "Solidity",
-    "sources": {
-        "Greeter.sol": {
-            "content": contractText
-        }
-    },
-    "settings":
-        {
-            "outputSelection": {
-                "*": {
-                    "*": [
-                        "metadata", "evm.bytecode"
-                        , "evm.bytecode.sourceMap"
-                    ]
-                }
-            }
-        }
-})
-bytecode = compiled_sol['contracts']['Greeter.sol']['Greeter']['evm']['bytecode']['object']
-abi = json.loads(compiled_sol['contracts']['Greeter.sol']['Greeter']['metadata'])['output']['abi']
-W3 = Web3(WebsocketProvider('wss://ropsten.infura.io/ws/v3/%s'%infuraKey))
-account1=Account.from_key(privateKey);
-print("account created")
-
+print("hello world")
+ 
